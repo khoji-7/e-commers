42308509639e3c552860864a18a8dfbf6d47d275
khoji-7 <nematxonovh@gmail.com>
2024-08-26 01:12:52 +0500

    add navbar

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import {Routes, Route} from "react-router-dom"
+import Collection from './pages/Collection'
+import Home from './pages/home'
+import About from './pages/about'
+import Cart from './pages/Cart'
+import Contact from './pages/Contact'
+import Order from './pages/order'
+import Placeholder from './pages/Placeholder'
+import Product from './pages/Product'
+import Login from './pages/Login'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <div className='px-4 sm:px-[5vw] ms:px-[7vw] lg:px-[9vw]'>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/collection' element={<Collection/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='contact' element={<Contact/>} />
+        <Route path='/order' element={<Order/>}/>
+        <Route path='placeholder' element={<Placeholder/>}/>
+        <Route path='/product' element={<Product/>}/>
+        <Route path='/login' element={<Login/>} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
