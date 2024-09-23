fe076fb97650219a3d3f8bd4d6ca536a2319083f
khoji-7 <nematxonovh@gmail.com>
2024-09-23 21:44:08 +0500

    add product id

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,37 @@
-import React from 'react'
-import {Routes, Route} from "react-router-dom"
-import Collection from './pages/Collection'
-import Home from "./pages/Home"
-import About from './pages/About'
-import Cart from './pages/Cart'
-import Contact from './pages/Contact'
-import Order from './pages/Order'
-import Placeholder from './pages/Placeholder'
-import Product from './pages/Product'
-import Login from './pages/Login'
-import Navbar from './components/Navbar'
-import "./index.css"
-import Footer from './components/Footer'
-import Searchbar from './components/Searchbar'
-
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Collection from './pages/Collection';
+import Home from './pages/Home';
+import About from './pages/About';
+import Cart from './pages/Cart';
+import Contact from './pages/Contact';
+import Order from './pages/Order';
+import Placeholder from './pages/Placeholder';
+import Product from './pages/Product';
+import Login from './pages/Login';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Searchbar from './components/Searchbar';
+import './index.css';
 
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] ms:px-[7vw] lg:px-[9vw]'>
-      <Navbar/>
-      <Searchbar/>
+      <Navbar />
+      <Searchbar />
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/collection' element={<Collection/>}/>
-        <Route path='/about' element={<About/>}/>
-        <Route path='/cart' element={<Cart/>}/>
-        <Route path='contact' element={<Contact/>} />
-        <Route path='/order' element={<Order/>}/>
-        <Route path='placeholder' element={<Placeholder/>}/>
-        <Route path='/product/:productId' element={<Product/>}/>
-        <Route path='/login' element={<Login/>} />
+        <Route path='/' element={<Home />} />
+        <Route path='/collection' element={<Collection />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/order' element={<Order />} />
+        <Route path='/placeholder' element={<Placeholder />} />
+        <Route path='/product/:productId' element={<Product />} />        <Route path='/login' element={<Login />} />
       </Routes>
-      <Footer/>
-    </div>
-  )
-}
+      <Footer />
+    </div> 
+  );
+};
 
-export default App
+export default App;
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,18 +1,74 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { ShopContext } from "../context/ShopContext";
+import { assets } from "../assets/frontend_assets/assets";
 
 const Product = () => {
+    const { productId } = useParams();    
+    const { products, currency} = useContext(ShopContext);
+    const [productData, setProductData] = useState(null);
+    const [image, setImage] = useState("");
 
+    const fetchProductData = () => {
+        const product = products.find((item) => item._id === productId);
+        if (product) {
+            setProductData(product);
+            setImage(product.image[0]);
+        }
+    };
 
-const { productId} = useParams();
+    useEffect(() => {
+        fetchProductData();
+    }, [productId, products]);
 
+    return productData ? (
+        <div className="border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100">
+            {/* Mahsulot ma'lumotlari */}
+            <div className="flex gap-12 sm:gap-12 flex-col sm:flex-row">
+                {/* Mahsulot rasm */}
+                <div className="flex-1 flex flex-col-reverse gap-3 sm:flex-row">
+                    <div className="flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full">
+                        {productData.image.map((item, index) => (
+                            <img
+                                onClick={()=>setImage(item)}
+                                src={item}
+                                key={index}
+                                className="w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer"
+                                alt={`Product image ${index}`}
+                            />
+                        ))}
+                    </div>
+                    <div className="w-full sm:w-[80%]">
+                            <img src={image} alt="" className="w-full h-auto"/>
+                    </div> 
+                </div>
+                {/* {----------------PRODUCT INFO-------------} */}
 
-  return (
-    <div>
-      
-    </div>
-  )
-}
+                <div className="flex-1">
+                    <h1 className="font-medium text-2xl mt-2">
+                        {productData.name}
+                    </h1>
+                    <div className="flex items-center gap-1 mt-2">
+                        <img src={assets.star_icon} alt="" className="w-3 5" />
+                        <img src={assets.star_icon} alt="" className="w-3 5" />
+                        <img src={assets.star_icon} alt="" className="w-3 5" />
+                        <img src={assets.star_icon} alt="" className="w-3 5" />
+                        <img src={assets.star_dull_icon} alt="" className="w-3 5" />
+                        <p className="pl-2 ">
+                            (123)
+                        </p>
+                    </div>
+                    <p className="mt-5 text-3xl font-medium">
+                        {currency}{productData.price}
+                    </p>
+                </div>
+                 
 
-export default Product
+            </div>
+        </div>
+    ) : (  
+        <div className="opacity-0">Loading...</div>
+    );
+};
  
+export default Product;
