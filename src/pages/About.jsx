31398f9dd8ccac7d150ddc28d196c3847dded0e5
khoji-7 <nematxonovh@gmail.com>
2024-09-26 19:51:45 +0500

    add about us

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
+import Title from '../components/Title'
+import { assets } from '../assets/frontend_assets/assets'
 
 const About = () => {
-  return (
-    <div>
-      About
-    </div>
-  )
+    return (
+        <div>
+            <div className='text-2xl text-center pt-8 border-t'>
+                <Title text1={"About"}  text2={"Us"}/>
+            </div>
+            <div className='my-10 flex flex-col md:flex-row gap-16'>
+                <img className='w-full md:max-w-[450px]' src={assets.about_img } alt="" />
+                <div className='flex flex-col justify-center gap-6 md:w-2/4' >
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Et nihil eius laborum exercitationem doloremque eaque reprehenderit, facere placeat iusto consequuntur! Adipisci iure voluptas distinctio porro tenetur, exercitationem assumenda modi quo minima illo id? Voluptatum assumenda illo natus quaerat eos inventore iusto voluptatibus quidem? A similique eaque architecto? Nulla ad mollitia, neque corrupti sequi, tenetur nam ab officia quia enim commodi, corporis illo iure tempora eveniet consequuntur accusantium doloribus quidem. Atque.
+                    </p>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae et, cupiditate pariatur corporis quis soluta. Officia, ea? Non eveniet dolores assumenda delectus quasi numquam vero porro quaerat facilis impedit voluptatem totam nostrum, fuga itaque modi error cum nisi doloribus. Excepturi quaerat est cupiditate quis perferendis officiis nobis? Minus, repellat iste.
+                    </p>
+                    <b className='text-gray-800'>
+                      Our Mission
+                    </b>
+                    <p>
+                      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque omnis quaerat voluptatum laborum vitae, esse, deserunt cupiditate enim explicabo amet consectetur tenetur, eum perferendis incidunt dolore sint odio natus culpa. Voluptatum porro accusantium tempora fuga, optio ad possimus ut animi.
+                    </p>
+                </div>
+            </div>
+
+        </div>
+    )
 }
 
 export default About
